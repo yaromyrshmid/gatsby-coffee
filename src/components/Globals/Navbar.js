8e15f31181c64e4f3338c2bca6959e2a65f0de38
yaromyrshmid <yaromyr.shmid@gmail.com>
2019-10-18 14:19:53 +0300

    navbar

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import { FaCartArrowDown } from "react-icons/fa"
+import { Navbar, Nav } from "react-bootstrap"
+
+import logo from "../../images/logo.svg"
+
+const Navigation = () => {
+  const [links, setLinks] = useState([
+    {
+      id: 1,
+      path: "/",
+      text: "home",
+    },
+    {
+      id: 2,
+      path: "/about",
+      text: "about",
+    },
+  ])
+
+  return (
+    <Navbar expand="sm" bg="light">
+      <Navbar.Brand>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+          {/* https://www.iconfinder.com/icons/185113/coffee_streamline_icon
+            Creative Commons (Attribution 3.0 Unported);
+            https://www.iconfinder.com/webalys */}
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav className="ml-auto mr-auto">
+          {links.map(link => {
+            return (
+              <Link
+                to={link.path}
+                key={link.id}
+                className="nav-link text-capitalize mt-2"
+              >
+                {link.text}
+              </Link>
+            )
+          })}
+          <Link to="/" className="nav-link ml-sm-5">
+            <FaCartArrowDown className="cart-icon" />
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Navigation
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import "./bootstrap.min.css"
+import Navigation from "./Globals/Navbar"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <Navigation />
+      {children}
+    </>
+  )
 }
 
 Layout.propTypes = {
